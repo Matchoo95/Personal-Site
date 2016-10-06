41c427dbee4216253f6fce5a044da32ccc61e4d3
Matchoo95 <mattmatth123@googlemail.com>
2016-10-06 18:18:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
-	include ("https://raw.githubusercontent.com/Matchoo95/Personal-Site/master/home.html?token=d55945e988862d466e1cf7bf20816d148afdd18a");
+	include ("https://raw.githubusercontent.com/Matchoo95/Personal-Site/master/home.html");
 ?>
