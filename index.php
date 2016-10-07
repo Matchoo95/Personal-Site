18a16838ff26ad00033f0d3b09549a57770c3ee1
Matchoo95 <mattmatth123@googlemail.com>
2016-10-07 10:38:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 	include ("https://raw.githubusercontent.com/Matchoo95/Personal-Site/master/home.html");
 ?>
+
+    <script type="text/javascript">
+        $( document ).ready(function() {
+            $('#content1').load('https://raw.githubusercontent.com/Matchoo95/Personal-Site/master/home.html').delay(100);
+        });
+    </script>
